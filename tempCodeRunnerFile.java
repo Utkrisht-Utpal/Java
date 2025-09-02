36d92cc0a754136247110356befc63e98e7f207a
Utkrisht Utpal <utkrishtutpal1@gmail.com>
2025-09-02 16:25:29 +0530

    Push zero to last

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,10 @@
+        // for(int i = 0; i < arr.length; i++){
+        //     if (arr[i] == 0){
+        //         arr[i] = arr[i+1];
+        //         arr[i] = 0;
+        //     }
+        // }
+        
+        // for(int i = 0; i < arr.length; i++){
+        //     System.out.print(arr[i]);
+        // }
