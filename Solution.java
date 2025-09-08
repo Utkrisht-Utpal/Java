1ed6de06bd4108b1e0d4ee79def28f761459c0b5
Utkrisht Utpal <utkrishtutpal1@gmail.com>
2025-09-08 21:44:48 +0530

    Learnt LinkedList in java

--- Solution.java
+++ Solution.java
@@ -1,25 +1,35 @@
-import java.util.Arrays;
-
 class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        if (n == 0) return 0;
 
-    static void pushZerosToEnd(int[] arr) {
-        
-        int count = 0;  
+        int minPrice = prices[0];
+        int bestProfit1 = 0;
+        int sellIndex = 0;
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != 0)
-                arr[count++] = arr[i];
+        for (int i = 1; i < n; i++) {
+            if (prices[i] - minPrice > bestProfit1) {
+                bestProfit1 = prices[i] - minPrice;
+                sellIndex = i;
+            }
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
         }
 
-        while (count < arr.length)
-            arr[count++] = 0;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 0, 4, 3, 0, 5, 0};
-        pushZerosToEnd(arr);
-        for (int num : arr) {
-            System.out.print(num + " ");
+        int bestProfit2 = 0;
+        if (sellIndex < n - 1) {
+            int minPrice2 = prices[sellIndex + 1];
+            for (int i = sellIndex + 1; i < n; i++) {
+                if (prices[i] - minPrice2 > bestProfit2) {
+                    bestProfit2 = prices[i] - minPrice2;
+                }
+                if (prices[i] < minPrice2) {
+                    minPrice2 = prices[i];
+                }
+            }
         }
+
+        return bestProfit1 + bestProfit2;
     }
 }
---
+++ linkedList.java
@@ -0,0 +1,56 @@
+public class linkedList {
+
+  static class Node{
+    int data;
+    Node next;
+
+    public Node(int data){
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  static class LinkedList{
+    Node head;
+    
+    public LinkedList(){
+      this.head = null;
+    }
+
+    void create(int data){
+      Node newNode = new Node(data);  
+      if(head == null){
+        head = newNode;
+      }
+
+      else{
+        Node temp = head;
+        while(temp.next != null){
+          temp = temp.next;
+        }
+        temp.next = newNode;
+      }
+    }
+
+    void printList(){
+      Node temp = head;
+      while(temp != null){
+        System.out.print(temp.data + " -> ");
+        temp = temp.next;
+      }
+      System.out.print("null");
+    }
+  }
+
+  public static void main(String[] args) {
+    Test outer = new Test();
+      LinkedList list = new LinkedList();
+
+      list.create(10);
+      list.create(20);
+      list.create(30);
+
+      System.out.println("Linked List elements:");
+      list.printList();
+  }
+}
