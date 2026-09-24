6a2e065104dd71e824fb35001a334a41cd8e6a42
Utkrisht Utpal <utkrishtutpal1@gmail.com>
2026-09-24 09:55:44 +0530

    practiced array in java

---
+++ Array.java
@@ -0,0 +1,10 @@
+public class Array {
+    void main () {
+        int arr[] = new int[6];
+
+        for (int i = 0; i < 6; i++) {
+            arr[i] = 2;
+            System.out.println(arr[i]);
+        }
+    }
+}
